c65eb33ae2ffb6aa94f6488f7ebb2a2ce1736074
Cleggatron <david.robert.clegg@gmail.com>
2022-03-10 08:12:04 +0000

    added negative number error

--- index.js
+++ index.js
@@ -8,8 +8,8 @@
 //then in multiples of 4 till 36
 
 function iterator(A, X){
-    if(isNaN(A) || isNaN(X)){
-        throw new Error("You have not provided valid arguements")
+    if(isNaN(A) || isNaN(X) || A <= 0 || X <= 0){
+        throw new Error("You must provide numeric positive values for both arguments")
     }
 
     const output = {
@@ -17,7 +17,7 @@ function iterator(A, X){
         two: [],
         three: []
     };
-
+    //the if statements will push values into our output objects
     for(let i = A, count = 1; i <= 3*X ; i+=A, count++){
         if(i <= X){
             output.one.push(i);
@@ -30,10 +30,11 @@ function iterator(A, X){
         }
     }
 
+    //Here we can get the array values from our objects as array items    
     const subArrays = Object.values(output);
-
+    //We can then iterate through and console log them all out.
     subArrays.forEach(subArray => {
-        console.log("New Array:")
+        console.log("New Iteration:")
         subArray.forEach(number => {
             console.log(number)
         })
@@ -42,7 +43,8 @@ function iterator(A, X){
     return output;
 }
 
-iterator(2, 14);
-iterator(3, 32);
+//iterator(2, 14);
+//iterator(3, 32);
+iterator(-1, -12);
 
 module.exports = iterator;
